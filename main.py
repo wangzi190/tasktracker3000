53fff3487602d86fd6134ac86ec42c0ade743abe
wangzi190 <gray.L.zhuang@gmail.com>
2022-07-16 19:19:12 -0700

    more front end setups

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-from flask import Flask
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, url_for, redirect, session
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from pathlib import Path
+from os import environ
+
 main = Flask(__name__, template_folder='templates')
-
-from logging import FileHandler,WARNING
-file_handler = FileHandler('errorlog.txt')
-file_handler.setLevel(WARNING)
-
 main.config.update(
     TESTING=True,
     DEBUG= True,
-    FLASK_ENV='development'
+    FLASK_ENV='development',
+    SQLALCHEMY_DATABASE_URI='sqlite:///students.sqlite3'
 )
+
+login_manager = LoginManager()
+loginApp = Flask(__name__)
+login_manager.init_app(loginApp)
+loginApp.config.update(
+    TESTING=True,
+    DEBUG= True,
+    FLASK_ENV='development',
+    SQLALCHEMY_DATABASE_URI='sqlite:///students.sqlite3'
+)
+loginApp.secret_key = environ.get('SECRET_KEY')
 
 @main.route("/")
 def index():
@@ -21,7 +31,26 @@ def index():
 def about():
     return render_template("/about.html")
 
+@main.route("/account/")
+def account():
+    return render_template("/logreg.html")
+
+""" @main.route('account', methods=['GET', 'POST'])
+def account():
+    if request.method == 'POST':
+        loginbool = request.form[loginbool]
+        signupbool = request.form[signupbool]
+        if loginbool != 0:
+            lusername = request.form[lusername]
+            lpassword = request.form[lpassword]
+            lremember = request.form[lremember]
+        if signupbool != 0:
+            susername = request.form[susername]
+            semail = request.form[semail]
+            spassword = request.form[spassword]
+            sconfirmpass = request.form[sconfirmpass]
+    return render_template("/logreg.html")
+    # return redirect(url_for('index')) """ 
+
 if __name__ == "__main__":
   main.run(debug=True)
-
-# py -u "c:\Users\Owner\myprojects\tasktracker3000\main.py"
