a87c29b53a5335e109925fbfeaf02fd88483dae7
wangzi190 <gray.L.zhuang@gmail.com>
2022-07-20 21:02:16 -0700

    signups working

--- account.py
+++ account.py
@@ -1,35 +1,37 @@
 from flask import Blueprint, render_template, request, flash, url_for, redirect
-from models import User, login_manager, db
-
 account = Blueprint('account', __name__, url_prefix='/account', template_folder='templates/account')
 
-@account.route("/signup/", methods=['GET', 'POST'])
-def signup():
-    if request.form:
-        username=request.form['username']
-        email=request.form['email']
-        password=request.form['password']
-        confirmpass=request.form['confirmpass']
-        dupeUser = User.query.filter(username=username).first()
-        if username and email and password:
-            if password == confirmpass:
-                if not dupeUser:
-                    newUser = User(
-                        username=username,
-                        email=email,
-                        password=password,
-                    )
-                    db.session.add(newUser)
-                    db.session.commit()
-                    redirect(url_for('account.login'))
-                else:
-                    flash('Username already taken. Please try again.')
-            else:
-                flash('Passwords do not match. Please try again.')
-        else:
-            flash('Missing required field. Please try again.')
-    return render_template("/signup.html")
-
-@account.route("/login/", methods=['GET', 'POST'])
+@account.route("/login/")
 def login():
     return render_template("/login.html")
+
+@account.route("/signup/")
+def signup():
+    return render_template("/signup.html")
+
+@account.route("/signup/", methods=['POST'])
+def signup_action():
+    from main import User, db
+    username=request.form['username']
+    email=request.form['email']
+    password=request.form['password']
+    confirmpass=request.form['confirmpass']
+    dupeUser = User.query.filter_by(username=str(username)).first()
+    if username and email and password:
+        if password == confirmpass:
+            if not dupeUser:
+                newUser = User(
+                    str(username),
+                    str(email),
+                    str(password),
+                )
+                db.session.add(newUser)
+                db.session.commit()
+                return redirect(url_for('account.login'))
+            else:
+                flash('Username already taken. Please try again.')
+        else:
+            flash('Passwords do not match. Please try again.')
+    else:
+        flash('Missing required field. Please try again.')
+    return render_template("/signup.html")
--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 from flask import Flask, render_template
-from account import account
+from flask_login import UserMixin, LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash
+
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 main = Flask(__name__, template_folder='templates')
+from account import account
 main.register_blueprint(account)
 main.config.update(
     TESTING=True,
     DEBUG= True,
     FLASK_ENV='development',
+    SECRET_KEY=os.environ.get('SECRET_KEY'),
+    SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI'),
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
 )
+
+login_manager = LoginManager()
+login_manager.init_app(main)
+login_manager.login_view = 'account.login'
+
+db = SQLAlchemy(main)
+db.init_app(main)
 
 @main.route("/")
 def index():
@@ -20,6 +39,40 @@ def about():
 @main.route("/tasks/")
 def tasks():
     return render_template("/tasks.html")
+
+class User(UserMixin, db.Model):
+    uid = db.Column(
+        db.Integer, 
+        primary_key=True,
+    )
+    username = db.Column(
+        db.String(256),
+        unique=True,
+        nullable=False,
+        index=True
+    )
+    email = db.Column(
+        db.String(256),
+        unique=False,
+        nullable=False,
+        index=True
+    )
+    password = db.Column(
+        db.String(256),
+        nullable=False
+    )
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = generate_password_hash(password, method='sha256')
+
+@login_manager.user_loader
+def load_user(uid):
+    return User.get(uid)
 
 if __name__ == "__main__":
   main.run(debug=True)
